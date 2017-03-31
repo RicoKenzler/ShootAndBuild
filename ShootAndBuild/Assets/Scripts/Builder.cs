82fed9d6a1231146f79850d4a1c782f86bc75369
DieDickeMutter <Johannes_e@web.de>
2017-03-31 21:37:43 +0200

    Construct Sounds

--- ShootAndBuild/Assets/Scripts/Builder.cs
+++ ShootAndBuild/Assets/Scripts/Builder.cs
@@ -5,6 +5,7 @@ public class Builder : MonoBehaviour
 	public GameObject towerPrefab;
 	public float distance = 2;
 
+	public AudioClip[] buildSounds;
 
 	public void Build()
 	{
@@ -14,6 +15,13 @@ public class Builder : MonoBehaviour
 		{
 			GameObject instance = Instantiate(towerPrefab);
 			instance.transform.position = pos;
+
+			if (buildSounds.Length > 0)
+            {
+                int rndSoundIndex = Random.Range(0, buildSounds.Length);
+                AudioClip rndSound = buildSounds[rndSoundIndex];
+                AudioManager.instance.PlayOneShot(rndSound, transform.position, 0.5f);
+            }
 		}
 	}
 }
