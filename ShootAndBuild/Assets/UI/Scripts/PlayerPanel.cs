8f4c31f1be8b4c8fe34af2f312abfb48e8716806
DieDickeMutter <Johannes_e@web.de>
2017-04-01 17:44:04 +0200

    UI WIP

---
+++ ShootAndBuild/Assets/UI/Scripts/PlayerPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPanel : MonoBehaviour
+{
+	[SerializeField]
+	GameObject healthBarFillImage;
+
+	[SerializeField]
+	GameObject activeItemImage;
+
+	[SerializeField]
+	GameObject activeWeaponImage;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public int health
+    {
+        get; set;
+    }
+
+	public GameObject activeItem
+    {
+        get; set;
+    }
+
+	public GameObject activeWeapon
+    {
+        get; set;
+    }
+
+	public static PlayerPanel instance
+    {
+        get; set;
+    }
+}
