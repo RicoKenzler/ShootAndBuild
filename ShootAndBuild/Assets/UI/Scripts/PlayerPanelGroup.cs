c25267517d0475aacd591ab4a24b2bc065b8987e
DieDickeMutter <Johannes_e@web.de>
2017-04-13 20:14:41 +0200

    Cleaned up PlayerPanel

--- ShootAndBuild/Assets/UI/Scripts/PlayerPanelGroup.cs
+++ ShootAndBuild/Assets/UI/Scripts/PlayerPanelGroup.cs
@@ -8,9 +8,6 @@ public class PlayerPanelGroup : MonoBehaviour
 	[SerializeField]
 	private PlayerPanel playerPanelPrefab;
 
-	public  float playerPanelAspectRatio		= 1.35f;
-	private float lastPlayerPanelAspectRatio	= 1.35f;
-
 	private Dictionary<PlayerID, PlayerPanel> playerPanels = new Dictionary<PlayerID, PlayerPanel>();
 
 	void Awake()
@@ -27,16 +24,7 @@ public class PlayerPanelGroup : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (lastPlayerPanelAspectRatio != playerPanelAspectRatio)
-		{
-			foreach (PlayerPanel panel in playerPanels.Values)
-			{
-				panel.GetComponent<PlayerPanelResizer>().aspectRatio = playerPanelAspectRatio;
-			}
-
-			LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
-			lastPlayerPanelAspectRatio = playerPanelAspectRatio;
-		}
+		
 	}
 
 	public void AddPlayerPanel(PlayerID playerID, GameObject player)
@@ -47,22 +35,22 @@ public class PlayerPanelGroup : MonoBehaviour
 			return;
 		}
 
-		GameObject newPlayerPanelObject = Instantiate(playerPanelPrefab.gameObject, gameObject.transform);
+		GameObject newPlayerPanelObject = Instantiate(playerPanelPrefab.gameObject, gameObject.transform, false);
 		newPlayerPanelObject.name = "Player Panel " + playerID;
 
 		RectTransform newPanelRect = newPlayerPanelObject.GetComponent<RectTransform>();
 		
-		newPanelRect.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		//newPanelRect.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
 		PlayerPanel newPlayerPanel = newPlayerPanelObject.GetComponent<PlayerPanel>();
 		newPlayerPanel.AssignPlayer(player);
 
 		playerPanels.Add(playerID, newPlayerPanel);
 
-		LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
-		LayoutRebuilder.MarkLayoutForRebuild(newPlayerPanel.transform as RectTransform);
+		//LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
+		//LayoutRebuilder.MarkLayoutForRebuild(newPlayerPanel.transform as RectTransform);
 
-		newPlayerPanel.GetComponent<PlayerPanelResizer>().aspectRatio = playerPanelAspectRatio;
+		//newPlayerPanel.GetComponent<PlayerPanelResizer>().aspectRatio = playerPanelAspectRatio;
 	}
 
 	public PlayerPanel GetPlayerPanel(PlayerID playerID)
