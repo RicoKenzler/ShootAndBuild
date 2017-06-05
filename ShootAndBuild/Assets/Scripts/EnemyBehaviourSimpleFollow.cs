37ed60aa92193942f4a4ba2924cb512542c50a71
DieDickeMutter <Johannes_e@web.de>
2017-06-05 18:58:51 +0200

    Enemies: ZigZag

--- ShootAndBuild/Assets/Scripts/EnemyBehaviourSimpleFollow.cs
+++ ShootAndBuild/Assets/Scripts/EnemyBehaviourSimpleFollow.cs
@@ -5,9 +5,31 @@ namespace SAB
 {
     public class EnemyBehaviourSimpleFollow : EnemyBehaviourBase
     {
+        public  float zigZagFrequency    = 0.0f;
+        public  float zigZagAngle        = 90.0f;
+        private int   zigZagID           = 0;
+
         protected override void Start()
         {
 			base.Start();
+
+            // TODO: Use other ID system
+            zigZagID = UnityEngine.Random.Range(0, 10000);
+        }
+
+        Vector3 ApplyZigZagAmount(Vector3 direction)
+        {
+            if (zigZagFrequency == 0.0f || zigZagAngle == 0.0f)
+            {
+                return direction;
+            }
+
+            float currentAngleNorm = Mathf.Sin(zigZagFrequency * Time.time + zigZagID);
+            float currentAngle = zigZagAngle * 0.5f * currentAngleNorm;
+
+            Quaternion rotation = Quaternion.AngleAxis(currentAngle, Vector3.up);
+
+            return rotation * direction;
         }
 
 		protected override void OnUpdate()
@@ -25,7 +47,9 @@ namespace SAB
             Vector3 directionTowardsTarget;
 			float distToTarget = GetDistanceTo(nearestTarget, out directionTowardsTarget);
 
-            transform.LookAt(nearestTarget.transform);
+            directionTowardsTarget = ApplyZigZagAmount(directionTowardsTarget);
+
+            transform.rotation = Quaternion.LookRotation(directionTowardsTarget);
 
             if (distToTarget > attackDistance)
             {
