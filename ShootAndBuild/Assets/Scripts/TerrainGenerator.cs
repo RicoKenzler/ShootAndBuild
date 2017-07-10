df2f9f42cd90a74995f41006deec37effb6c2f7e
DieDickeMutter <Johannes_e@web.de>
2017-07-10 20:33:22 +0200

    TerrainGenerator: Texture Enhancements

--- ShootAndBuild/Assets/Scripts/TerrainGenerator.cs
+++ ShootAndBuild/Assets/Scripts/TerrainGenerator.cs
@@ -5,9 +5,10 @@ using UnityEditor;
 
 namespace SAB
 {
-	enum TerrainTextures
+	public enum TerrainTexturePairs
 	{
-		Grass,
+		PlaneA,
+		PlaneB,
 		Rock,
 
 		Count
@@ -43,26 +44,70 @@ namespace SAB
 	}
 
 	[System.Serializable]
-	public class TextureParameters
+	public class TexturePair
 	{
-		public Texture2D GrassAlbedo;
-		public Texture2D GrassNormal;
+		public Texture2D Albedo1;
+		public Texture2D Albedo2;
 
-		public Texture2D RockAlbedo;
-		public Texture2D RockNormal;
-
-		[Range(0.5f, 20.0f)]
-		public float GrassTiling		= 4.0f;
+		public Texture2D Normal1;
+		public Texture2D Normal2;
 
 		[Range(0.5f, 20.0f)]
-		public float RockTiling			= 4.0f;
+		public float Tiling1 = 4.0f;
+		
+		[Range(0.5f, 20.0f)]
+		public float Tiling2 = 4.0f;
 
 		[Range(1.0f, 20.0f)]
-		public float BlendingSharpness	= 7.0f;
+		public float BlendingSharpness12 = 7.0f;
+
+		[Range(0.005f, 1.0f)]
+		public float PerlinFrequency12	= 0.015f;
+
+		[Range(0.000f, 1.0f)]
+		public float Share2ndTexture = 0.4f;
+
+		public SplatPrototype CreateSplatPrototype(bool second)
+		{
+			SplatPrototype prototype = new SplatPrototype();
+
+			prototype.texture	= second ? Albedo2 : Albedo1;
+			prototype.normalMap = second ? Normal2 : Normal1;
+
+			float tiling = second ? Tiling2 : Tiling1;
+			prototype.tileSize	= new Vector2(tiling, tiling);
+
+			return prototype;
+		}
+
+		public float GetBlendValue12(float seed, float x, float z)
+		{
+			float perlinCoarse = Mathf.PerlinNoise(seed + x * PerlinFrequency12, seed + z * PerlinFrequency12);
+			
+			float amount2nd = TerrainGenerator.RedistributeBlendFactor(perlinCoarse, Share2ndTexture);
+
+			return TerrainGenerator.ApplySharpness(amount2nd, BlendingSharpness12);
+		}
+	}
+
+	[System.Serializable]
+	public class TextureParameters
+	{
+		public TexturePair TexturePlaneA;
+		public TexturePair TexturePlaneB;
+		public TexturePair TextureRock;
+		
+		[Range(1.0f, 20.0f)]
+		public float RockBlendingSharpness	= 7.0f;
+
+		[Range(0.0f, 1.0f)]
+		public float PlaneBShare = 0.5f;
 
 		[Range(0.0f, 90.0f)]
 		public float RockSteepnessAngle	= 55.0f;
 	}
+
+	// -----------------------------------------------------------------
 
 	public class TerrainGenerator : MonoBehaviour
 	{
@@ -77,11 +122,14 @@ namespace SAB
 		
 		GameObject TerrainObject;
 
-		// Use this for initialization
-		void Start ()
+		// -----------------------------------------------------------------
+
+		public int TexturePairToSplatIndex(TerrainTexturePairs pair, bool second)
 		{
-			
+			return ((int) pair) * 2 + (second ? 1 : 0);
 		}
+
+		// -----------------------------------------------------------------
 
 		public void DeleteTerrain()
 		{
@@ -103,6 +151,8 @@ namespace SAB
 				}
 			}
 		}
+		
+		// -----------------------------------------------------------------
 
 		public void RegenerateAll()
 		{
@@ -125,32 +175,33 @@ namespace SAB
 				TransformParams.TerrainSizeWS.y);
 
 			//Set textures
-			SplatPrototype Grass	= new SplatPrototype();
-			SplatPrototype Rock		= new SplatPrototype();
+			TerrainData.splatPrototypes = new SplatPrototype[] 
+			{
+				TextureParams.TexturePlaneA.CreateSplatPrototype(false),
+				TextureParams.TexturePlaneA.CreateSplatPrototype(true),
+				TextureParams.TexturePlaneB.CreateSplatPrototype(false),
+				TextureParams.TexturePlaneB.CreateSplatPrototype(true),
+				TextureParams.TextureRock.CreateSplatPrototype(false),
+				TextureParams.TextureRock.CreateSplatPrototype(true),
+			};
 
-			Grass.texture	= TextureParams.GrassAlbedo;
-			Grass.normalMap = TextureParams.GrassNormal;
-			Grass.tileSize	= new Vector2(TextureParams.GrassTiling, TextureParams.GrassTiling);
-
-			Rock.texture	= TextureParams.RockAlbedo;
-			Rock.normalMap	= TextureParams.RockNormal;
-			Rock.tileSize	= new Vector2(TextureParams.RockTiling, TextureParams.RockTiling);
-			
-			TerrainData.splatPrototypes = new SplatPrototype[] { Grass, Rock };
 			TerrainData.RefreshPrototypes();
 			TerrainData.SetAlphamaps(0, 0, GenerateTextureMaps(TerrainData));
 
 			//Create terrain
 			TerrainObject = Terrain.CreateTerrainGameObject(TerrainData);
+
 			TerrainObject.transform.position = new Vector3(
 				TransformParams.TerrainCenter.x - TransformParams.TerrainSizeWS.x * 0.5f, 
 				TransformParams.HeightMin, 
 				TransformParams.TerrainCenter.y - TransformParams.TerrainSizeWS.y * 0.5f);
+
 			TerrainObject.GetComponent<Terrain>().Flush();
 
 			TerrainObject.transform.parent = this.transform;
 		}
 
+		// -----------------------------------------------------------------
 
 		public float[,] GenerateHeightMap()
 		{
@@ -190,10 +241,49 @@ namespace SAB
 			return Heightmap;
 		}
 
+		// -----------------------------------------------------------------
+	
+		public static float RedistributeBlendFactor(float blend12, float share2)
+		{
+			if (share2 == 0.0f) { return 0.0f; }
+			if (share2 == 1.0f) { return 1.0f; }
+
+			float amount1st = Mathf.InverseLerp(Mathf.Max(2.0f * share2 - 1.0f, 0.0f), Mathf.Min(2.0f * share2, 1.0f), blend12);
+
+			return 1.0f - amount1st;
+		}
+
+		
+
+		public static float ApplySharpness(float oldBlendValue, float blendingSharpness)
+		{
+			if (oldBlendValue < 0.5f)
+			{
+				return Mathf.Pow(oldBlendValue * 2.0f, blendingSharpness) * 0.5f;
+			}
+			
+			return 1.0f - (Mathf.Pow((1.0f - oldBlendValue) * 2.0f, blendingSharpness) * 0.5f);
+		}
+
+		// -----------------------------------------------------------------
+
+		public float GetAmountPlaneA(int x, int z)
+		{
+			// We later want to get this from District Map
+			float plane2Amount = Mathf.PerlinNoise(Seed + 21 + x * 0.01f, Seed + 21 + z * 0.01f);
+
+			plane2Amount = RedistributeBlendFactor(plane2Amount, TextureParams.PlaneBShare);
+
+			plane2Amount = ApplySharpness(plane2Amount, 9.0f);
+
+			return 1.0f - plane2Amount;
+		}
+
+		// -----------------------------------------------------------------
 
 		public float[,,] GenerateTextureMaps(TerrainData TerrainData)
 		{
-			float[,,] textureMaps = new float[Resolution, Resolution, (int) TerrainTextures.Count];
+			float[,,] textureMaps = new float[Resolution, Resolution, (int) TerrainTexturePairs.Count * 2];
 
 			for (int x = 0; x < Resolution; x++)
 			{
@@ -202,25 +292,42 @@ namespace SAB
 					float normalizedX = (float)x / ((float)Resolution - 1f);
 					float normalizedZ = (float)z / ((float)Resolution - 1f);
 
-					float steepness = TextureParams.RockSteepnessAngle == 0.0f ? 0.0f : TerrainData.GetSteepness(normalizedX, normalizedZ) / TextureParams.RockSteepnessAngle;
+					float steepness = (TextureParams.RockSteepnessAngle == 0.0f) ? 1.0f : TerrainData.GetSteepness(normalizedX, normalizedZ) / TextureParams.RockSteepnessAngle;
+					steepness = Mathf.Min(steepness, 1.0f);
 
-					float rockAmount = steepness;
+					float rockAmount = ApplySharpness(steepness, TextureParams.RockBlendingSharpness);
 
-					if (rockAmount < 0.5f)
+					float blend12 = TextureParams.TextureRock.GetBlendValue12(Seed + 10, x, z);
+					textureMaps[z, x, TexturePairToSplatIndex(TerrainTexturePairs.Rock, false)]	= rockAmount * (1.0f - blend12);
+					textureMaps[z, x, TexturePairToSplatIndex(TerrainTexturePairs.Rock, true)]	= rockAmount * blend12;
+
+					float planeAmount = 1.0f - rockAmount;
+
+					float planeAAmount = planeAmount * GetAmountPlaneA(x, z);
+					float planeBAmount = planeAmount - planeAAmount;
+
+					blend12 = TextureParams.TexturePlaneA.GetBlendValue12(Seed + 20, x, z);
+					textureMaps[z, x, TexturePairToSplatIndex(TerrainTexturePairs.PlaneA, false)]	= planeAAmount * (1.0f - blend12);
+					textureMaps[z, x, TexturePairToSplatIndex(TerrainTexturePairs.PlaneA, true)]	= planeAAmount * blend12;
+
+					blend12 = TextureParams.TexturePlaneB.GetBlendValue12(Seed + 20, x, z);
+					textureMaps[z, x, TexturePairToSplatIndex(TerrainTexturePairs.PlaneB, false)]	= planeBAmount * (1.0f - blend12);
+					textureMaps[z, x, TexturePairToSplatIndex(TerrainTexturePairs.PlaneB, true)]	= planeBAmount * blend12;
+
+					float weightSum = 0.0f;
+
+					for (int t = 0; t < (int) TerrainTexturePairs.Count * 2; ++t)
 					{
-						rockAmount = Mathf.Pow(rockAmount * 2.0f, TextureParams.BlendingSharpness) * 0.5f;
-					}
-					else
-					{
-						rockAmount = 1.0f - (Mathf.Pow((1.0f - rockAmount) * 2.0f, TextureParams.BlendingSharpness) * 0.5f);
+						weightSum += textureMaps[z, x, t];
 					}
 
-					textureMaps[z, x, (int) TerrainTextures.Rock]	= rockAmount;
-					textureMaps[z, x, (int) TerrainTextures.Grass]	= 1.0f - rockAmount;
+					Debug.Assert(Mathf.Abs(weightSum - 1.0f) < 0.01f);
 				}
 			}
 			return textureMaps;
 		}
+
+		// -----------------------------------------------------------------
 
 		public float GetNormalizedHeight(float x, float z)
 		{
@@ -234,6 +341,8 @@ namespace SAB
 		}
 
 	}
+
+	// -----------------------------------------------------------------
 
 	[CustomEditor(typeof(TerrainGenerator))]
 	public class TerrainGeneratorEditor : Editor
