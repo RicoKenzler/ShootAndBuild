2b975f4ff95b39c42e8c0df9eac7f79824ed718d
DieDickeMutter <Johannes_e@web.de>
2017-10-31 16:58:12 +0100

    CodeConvention: 15/n

--- ShootAndBuild/Assets/Scripts/ScriptableObjects/BuffData.cs
+++ ShootAndBuild/Assets/Scripts/ScriptableObjects/BuffData.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SAB
 {
@@ -8,14 +9,33 @@ namespace SAB
 		DamageIncrease
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+
 	[CreateAssetMenu(menuName = "Custom/BuffData", fileName = "BuffData")]
 	public class BuffData : ScriptableObject
 	{
-		public BuffType type = BuffType.Slow;
-		public float duration = 0;
-		public float damageMultiplier = 0;
-		public float speedMultiplier = 0;
-		public GameObject visual = null;
+		[FormerlySerializedAs("type")]
+		[SerializeField] private BuffType m_Type = BuffType.Slow;
+
+		[FormerlySerializedAs("duration")]
+		[SerializeField] private float m_Duration = 0;
+
+		[FormerlySerializedAs("damageMultiplier")]
+		[SerializeField] private float m_DamageMultiplier = 0;
+
+		[FormerlySerializedAs("speedMultiplier")]
+		[SerializeField] private float m_SpeedMultiplier = 0;
+
+		[FormerlySerializedAs("visual")]
+		[SerializeField] private GameObject m_Visual = null;
+
+		///////////////////////////////////////////////////////////////////////////
+
+		public BuffType type				{ get { return m_Type; } }
+		public float	speedMultiplier		{ get { return m_SpeedMultiplier; } }
+		public float	damageMultiplier	{ get { return m_DamageMultiplier; } }
+
+		///////////////////////////////////////////////////////////////////////////
 
 		private float livingTime = 0;
 		private GameObject visualInstance = null;
@@ -25,14 +45,14 @@ namespace SAB
 		public void Update()
 		{
 			livingTime += Time.deltaTime;
-			livingTime = Mathf.Clamp(livingTime, 0, duration);
+			livingTime = Mathf.Clamp(livingTime, 0, m_Duration);
 		}
 
 		///////////////////////////////////////////////////////////////////////////
 
 		public float Progress
 		{
-			get { return livingTime / duration; }
+			get { return livingTime / m_Duration; }
 		}
 
 		///////////////////////////////////////////////////////////////////////////
@@ -53,12 +73,14 @@ namespace SAB
 
 		bool IsPositiveBuff()
 		{
-			return (damageMultiplier > 0 || speedMultiplier > 0);
+			return (m_DamageMultiplier > 0 || m_SpeedMultiplier > 0);
 		}
+
+		///////////////////////////////////////////////////////////////////////////
 
 		public void OnAdd(GameObject parent)
 		{
-			visualInstance = Instantiate(visual, parent.transform);
+			visualInstance = Instantiate(m_Visual, parent.transform);
 			visualInstance.transform.localPosition = Vector3.zero;
 
 			if (parent.GetComponent<InputController>())
--- ShootAndBuild/Assets/Scripts/ScriptableObjects/ItemData.cs
+++ ShootAndBuild/Assets/Scripts/ScriptableObjects/ItemData.cs
@@ -1,24 +1,48 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SAB
 {
 	[CreateAssetMenu(menuName = "Custom/ItemData", fileName = "ItemData")]
     public class ItemData : ScriptableObject
     {
-        public ItemType itemType = ItemType.None;
-        public ItemUsageCategory usageCategory = ItemUsageCategory.PassiveItem; //< what should we do with usagePrefab on itemUsage?
+		[FormerlySerializedAs("itemType")]
+        [SerializeField] private ItemType m_ItemType = ItemType.None;
 
-        public GameObject usagePrefab;								//< which item should we throw/use/activate on itemUsage?
+		[FormerlySerializedAs("usageCategory")]
+        [SerializeField] private ItemUsageCategory m_UsageCategory = ItemUsageCategory.PassiveItem; //< what should we do with usagePrefab on itemUsage?
 
-        public bool isShared = false;                               //< does item go into shared inventory?
-        public int initialCount = 0;                                //< whith how many items does player start?
-        public bool useOnCollect = false;                           //< is the item used instantanously when collected?
-        public Sprite icon;                                         //< icon to show in UI
+		[FormerlySerializedAs("usagePrefab")]
+        [SerializeField] private GameObject m_UsagePrefab;								//< which item should we throw/use/activate on itemUsage?
 
-        public WeaponData weaponData = null;                        // if the item is a wepon this is the weapon meta data
+		[FormerlySerializedAs("isShared")]
+        [SerializeField] private bool m_IsShared = false;                               //< does item go into shared inventory?
 
-		public List<BuffData> buffs;
+		[FormerlySerializedAs("initialCount")]
+        [SerializeField] private int m_InitialCount = 0;                                //< whith how many items does player start?
+
+		[FormerlySerializedAs("useOnCollect")]
+        [SerializeField] private bool m_UseOnCollect = false;                           //< is the item used instantanously when collected?
+
+		[FormerlySerializedAs("icon")]
+        [SerializeField] private Sprite m_Icon;                                         //< icon to show in UI
+
+		[FormerlySerializedAs("weaponData")]
+        [SerializeField] private WeaponData m_WeaponData = null;                        // if the item is a wepon this is the weapon meta data
+
+		[FormerlySerializedAs("buffs")]
+		[SerializeField] private List<BuffData> m_Buffs;
+		
+		public ItemType				itemType		{ get { return m_ItemType; } }
+		public ItemUsageCategory	usageCategory	{ get { return m_UsageCategory; } }
+		public GameObject			usagePrefab		{ get { return m_UsagePrefab; } }
+		public bool					isShared		{ get { return m_IsShared; } }
+		public int					initialCount	{ get { return m_InitialCount; } }
+		public bool					useOnCollect	{ get { return m_UseOnCollect; } }
+		public Sprite				icon			{ get { return m_Icon; } }
+		public WeaponData			weaponData		{ get { return m_WeaponData; } }
+		public List<BuffData>		buffs			{ get { return m_Buffs; } }
     }
 
 }
